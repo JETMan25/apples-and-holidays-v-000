1ca9466208d31977d20876a96286de0f233e6a70
jontenthoff@hotmail.com <jontenthoff@hotmail.com>
2018-09-11 20:31:32 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,8 +47,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:winter][:holiday_name][:supplies].collect do |holiday, supply|,
-supply.flatten
+  holiday_hash[:winter].collect do |holiday, supply|
+supply
 
 end
 end
